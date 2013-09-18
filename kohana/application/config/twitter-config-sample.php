6d42d08176e3afc2cdf8bd773f89ffd490de81f0
Michael Pierre <kaizencode@gmail.com>
2013-09-18 01:04:24 -0400

    Add sample Twitter config file

---
+++ kohana/application/config/twitter-config-sample.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die ('No direct script access');
+
+return array(
+	'setting' => 'value',
+	'tokens' => array(
+		'oauth_access_token' => 'xxxxxxxx',
+		'oauth_access_token_secret' => 'xxxxxxxx',
+		'consumer_key' => 'xxxxxxxx',
+		'consumer_secret' => 'xxxxxxxx', 
+	),
+);
+// End twitter-config-sample.php
